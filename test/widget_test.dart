df6fb5b0dafbe2ab08a9b6156bd11b4524365c8f
Ram Delatina <r2am9d@gmail.com>
2022-04-22 16:16:12 +0800

    Added "WidgetsFlutterBinding"

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,12 +5,14 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'package:vendibase/main.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   testWidgets('Check if ProviderScope is present', (WidgetTester tester) async {
     await tester.pumpWidget(const MyApp());
     expect(find.byType(ProviderScope), findsOneWidget);
